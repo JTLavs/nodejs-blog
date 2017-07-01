53d35bf52d90b25f527872932d277d59773b241e
James Lavery <mickey_noo@hotmail.com>
2017-07-01 15:15:37 +0100

    Added the mongoDb provider and adding functionality to add new posts and comment on individual posts

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
 var app = express();
 
@@ -19,7 +19,7 @@ if(process.env.NODE_ENV == 'development')
 if(process.env.NODE_ENV == 'production')
 	app.use(express.errorHandler());
 
-var articleProvider = new ArticleProvider();
+var articleProvider = new ArticleProvider('localhost', 27017)
 
 app.get('/', function(req,res){
 	articleProvider.findAll( function(error, docs){
@@ -30,4 +30,43 @@ app.get('/', function(req,res){
 	});
 })
 
+app.get('/blog/new', function(req,res){
+	res.render('blog_new.pug', { locals : { 
+		title : 'New Post'
+	}
+	});
+});
+
+app.get('/blog/:id', function(req, res){
+	console.log(req.params.id)
+	articleProvider.findById(req.params.id, function(error, article){
+		console.log(article)
+		res.render('blog_show.pug', 
+		{
+			title : article.title,
+			article:article
+		})
+	})
+})
+
+app.post('/blog/new', function(req, res){
+	articleProvider.save({
+		title: req.body.title,
+		body: req.body.body
+	}, function(err, docs){
+		res.redirect('/')
+	});
+});
+
+app.post('blog/addComment', function(req,res){
+	articleProvider.addCommentToArticle(req.body._id, {
+		person : req.body.person,
+		comment : req.body.comment,
+		created_at : new Date()
+	}, function(error, docs){
+		res.redirect('/blog/' + req.body._id)
+	})
+})
+
 app.listen(3000);
+console.log('Express server listening on port %d in %s mode', app.port, app.settings.env);
